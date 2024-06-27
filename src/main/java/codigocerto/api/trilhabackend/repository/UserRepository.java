01eab311710007e5cb71f28ee563ef45d93854cd
dyonne <bydyonne@gmail.com>
2024-06-27 08:37:29 -0300

    Criação do UserRepository

---
+++ src/main/java/codigocerto/api/trilhabackend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package codigocerto.api.trilhabackend.repository;
+
+import codigocerto.api.trilhabackend.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByLogin(String login);
+}
