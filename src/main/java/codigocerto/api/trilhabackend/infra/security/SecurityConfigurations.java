df6e4718f4a82801a32158b0d9f80f067e7ef676
dyonne <bydyonne@gmail.com>
2024-06-27 09:32:55 -0300

    Configuracao do BCrypt para password

--- src/main/java/codigocerto/api/trilhabackend/infra/security/SecurityConfigurations.java
+++ src/main/java/codigocerto/api/trilhabackend/infra/security/SecurityConfigurations.java
@@ -3,9 +3,13 @@ package codigocerto.api.trilhabackend.infra.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,6 +26,18 @@ public class SecurityConfigurations {
                         .anyRequest().authenticated()
                 )
                 .build();
+    }
+
+    // Método utilizado para recuperar o AuthenticationManager()
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    // faz a criptografia da senha via HASH antes de salvar no BD.
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
 
