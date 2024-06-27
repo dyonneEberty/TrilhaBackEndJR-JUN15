70604e49a70d18c73c82fa7984aa22c11bdd79a8
dyonne <bydyonne@gmail.com>
2024-06-27 08:37:11 -0300

    Criação da ENUM para ROLES

---
+++ src/main/java/codigocerto/api/trilhabackend/domain/user/UserRole.java
@@ -0,0 +1,17 @@
+package codigocerto.api.trilhabackend.domain.user;
+
+public enum UserRole {
+
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole(){
+        return role;
+    }
+}
