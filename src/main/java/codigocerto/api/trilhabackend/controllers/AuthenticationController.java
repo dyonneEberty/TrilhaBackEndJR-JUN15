ea6834ef01a78de21d62a91d37a8b800e2a8eab0
dyonne <bydyonne@gmail.com>
2024-06-28 08:24:48 -0300

    upd Endpoint "/register" encryptedPassword

---
+++ src/main/java/codigocerto/api/trilhabackend/controllers/AuthenticationController.java
@@ -0,0 +1,63 @@
+package codigocerto.api.trilhabackend.controllers;
+
+import codigocerto.api.trilhabackend.domain.user.AuthenticationDTO;
+import codigocerto.api.trilhabackend.domain.user.RegisterDTO;
+import codigocerto.api.trilhabackend.domain.user.User;
+import codigocerto.api.trilhabackend.repository.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class AuthenticationController {
+
+    @Autowired
+    UserRepository repository;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
+
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO data) {
+        if (this.repository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.login(), encryptedPassword, data.role());
+
+        this.repository.save(newUser);
+
+        return ResponseEntity.ok().build();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
