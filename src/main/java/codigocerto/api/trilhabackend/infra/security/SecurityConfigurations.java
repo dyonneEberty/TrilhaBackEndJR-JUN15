b5299a7f124b03cc8cdb9717bf1f0a70c996a104
dyonne <bydyonne@gmail.com>
2024-06-28 14:33:11 -0300

    Alteração na ordem dos filtros para para aplicar o securityFilter

--- src/main/java/codigocerto/api/trilhabackend/infra/security/SecurityConfigurations.java
+++ src/main/java/codigocerto/api/trilhabackend/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package codigocerto.api.trilhabackend.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,12 +12,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
-    // Anotação que possibilita o Spring instanciar a classe
+    @Autowired
+    SecurityFilter securityFilter;
+
+    // Corrente de filtros que será aplicada na requisicao, para fazer a seguraça da aplicacao
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
@@ -28,6 +33,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST, "/task").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
